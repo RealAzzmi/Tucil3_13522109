38d8f7af8316afc8def2ce9df130d27992a9c551
RealAzzmi <azmibazeid@gmail.com>
2024-05-04 12:30:14 +0700

    feat: use the standard dictionary

--- src/wordladder/dictionary/process.py
+++ src/wordladder/dictionary/process.py
@@ -1,5 +1,20 @@
+import os
+import glob
+
+# Delete previous files
+
+pattern = 'src/wordladder/dictionary/word_size_*.txt'
+files_to_delete = glob.glob(pattern)
+
+for file in files_to_delete:
+    os.remove(file)
+    print(f"Deleted file: {file}")
+
+# Create new files
+
 d = dict()
-with open('words_alpha.txt') as file:
+
+with open('src/wordladder/dictionary/dictionary_asisten.txt') as file:
     for line in file:
         line = line.strip()
 
@@ -9,6 +24,6 @@ with open('words_alpha.txt') as file:
             d[len(line)] = [line]
 
 for k, v in d.items():
-    with open('word_size_' + str(k) + '.txt', 'x') as file:
+    with open('src/wordladder/dictionary/word_size_' + str(k) + '.txt', 'x') as file:
         file.write('\n'.join(v))
     
