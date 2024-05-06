335cc3400547584128c52ec691376774afcd6f9a
RealAzzmi <azmibazeid@gmail.com>
2024-05-06 16:17:55 +0700

    refactor: remove unnecessary imports

--- src/wordladder/Process.java
+++ src/wordladder/Process.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
